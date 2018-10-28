5d626ab125a68afcc2716b7d7f84988c1a90c2ff
Abhilash <abhilash9026@outlook.com>
2018-10-28 22:28:53 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "abhilashm9026@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $mobile = $_POST['mobile'];
+    $subject = "Enquiry Mail";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'] ."\n\n" . "Can be contacted at" . $mobile;
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
